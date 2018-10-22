decb9ae0fcd6a8c7b2998fc37b666bba39f47046
Bear Su <timfanda35@gmail.com>
2018-10-22 14:54:36 +0800

    Move main process to export service

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
-// https://github.com/googleapis/google-api-go-client/tree/master/monitoring/v3
-// https://godoc.org/golang.org/x/oauth2/google#DefaultClient
-
 import (
-	"stackdriver-monitoring-exporter/pkg/gcp/stackdriver"
 	"stackdriver-monitoring-exporter/pkg/metric_exporter"
-	"stackdriver-monitoring-exporter/pkg/utils"
+	"stackdriver-monitoring-exporter/pkg/service"
 )
 
 var monitoringMetrics = []string{
@@ -15,33 +11,9 @@ var monitoringMetrics = []string{
 	"compute.googleapis.com/instance/network/received_bytes_count",
 }
 
-////////////////////////////////////////////////////////////////
-// Main Function
-
 func main() {
-	var c utils.Conf
-	c.LoadConfig()
+	exportService := service.ExportService{}
+	exportService.Exporter = metric_exporter.FileExporter{}
 
-	client := stackdriver.MonitoringClient{}
-	exporter := metric_exporter.FileExporter{}
-
-	client.SetTimezone(8)
-
-	for prjIdx := range c.Projects {
-		projectID := c.Projects[prjIdx].ProjectID
-
-		for mIdx := range monitoringMetrics {
-			metric := monitoringMetrics[mIdx]
-
-			instanceNames := client.GetInstanceNames(projectID, metric)
-
-			for instIdx := range instanceNames {
-				instanceName := instanceNames[instIdx]
-
-				points := client.RetrieveMetricPoints(projectID, metric, instanceName)
-
-				exporter.Export(client.StartTime.In(client.Location()), projectID, metric, instanceName, points)
-			}
-		}
-	}
+	exportService.Do()
 }
---
+++ pkg/service/export_service.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"stackdriver-monitoring-exporter/pkg/gcp/stackdriver"
+	"stackdriver-monitoring-exporter/pkg/metric_exporter"
+	"stackdriver-monitoring-exporter/pkg/utils"
+)
+
+var monitoringMetrics = []string{
+	"compute.googleapis.com/instance/cpu/usage_time",
+	"compute.googleapis.com/instance/network/sent_bytes_count",
+	"compute.googleapis.com/instance/network/received_bytes_count",
+}
+
+type ExportService struct {
+	conf utils.Conf
+	Exporter metric_exporter.MetricExporter
+}
+
+func (es ExportService) Do () {
+	var c utils.Conf
+	c.LoadConfig()
+
+	client := stackdriver.MonitoringClient{}
+
+	client.SetTimezone(8)
+
+	for prjIdx := range c.Projects {
+		projectID := c.Projects[prjIdx].ProjectID
+
+		for mIdx := range monitoringMetrics {
+			metric := monitoringMetrics[mIdx]
+
+			instanceNames := client.GetInstanceNames(projectID, metric)
+
+			for instIdx := range instanceNames {
+				instanceName := instanceNames[instIdx]
+
+				points := client.RetrieveMetricPoints(projectID, metric, instanceName)
+
+				es.Exporter.Export(client.StartTime.In(client.Location()), projectID, metric, instanceName, points)
+			}
+		}
+	}
+}
