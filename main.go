fb027b2b06b93b73b2f916e4c22291f7c72f6aee
Bear Su <timfanda35@gmail.com>
2018-10-22 14:05:21 +0800

    Simplize the export process
    - export all instances metrics of the project
    - set the export directory path

--- main.go
+++ main.go
@@ -8,12 +8,19 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	"stackdriver-monitoring-exporter/pkg/gcp/stackdriver"
 	. "stackdriver-monitoring-exporter/pkg/utils"
 )
 
 const dir = "metrics"
+
+var monitoringMetrics = []string{
+	"compute.googleapis.com/instance/cpu/usage_time",
+	"compute.googleapis.com/instance/network/sent_bytes_count",
+	"compute.googleapis.com/instance/network/received_bytes_count",
+}
 
 func saveTimeSeriesToCSV(filename string, metricPoints []string) {
 	log.Printf("Points len: %d", len(metricPoints))
@@ -32,6 +39,18 @@ func saveToFile(filename, header, content string) {
 	fmt.Fprintf(file, content)
 }
 
+
+func export(dateTime time.Time, projectID, metric, instanceName string, metricPoints []string) {
+	folder := fmt.Sprintf("%s/%s/%d/%2d/%2d/%s", dir, projectID, dateTime.Year(), dateTime.Month(), dateTime.Day(), instanceName)
+	os.MkdirAll(folder, os.ModePerm)
+
+	title := strings.Replace(metric, "compute.googleapis.com/instance/", "", -1)
+	title = strings.Replace(title, "/", "_", -1)
+
+	output := fmt.Sprintf("%s/%s[%s].csv", folder, dateTime.Format("2006-01-02T15:04:05"), title)
+	saveTimeSeriesToCSV(output, metricPoints)
+}
+
 ////////////////////////////////////////////////////////////////
 // Main Function
 
@@ -41,17 +60,23 @@ func main() {
 
 	client := stackdriver.MonitoringClient{}
 
-	for i := range c.Projects {
-		project := c.Projects[i]
-		for j := range project.Metrics {
-			metric := project.Metrics[j]
-			metric.LoadConfig()
+	client.SetTimezone(8)
 
-			points := client.RetrieveMetricPoints(project.ProjectID, &metric)
+	for prjIdx := range c.Projects {
+		projectID := c.Projects[prjIdx].ProjectID
 
-			dateTime := metric.StartTime.In(metric.Location()).Format("2006-01-02T15:04:05")
-			output := fmt.Sprintf("%s/%s[%s][%s].csv", dir, dateTime, metric.Title, metric.Unit)
-			saveTimeSeriesToCSV(output, points)
+		for mIdx := range monitoringMetrics {
+			metric := monitoringMetrics[mIdx]
+
+			instanceNames := client.GetInstanceNames(projectID, metric)
+
+			for instIdx := range instanceNames {
+				instanceName := instanceNames[instIdx]
+
+				points := client.RetrieveMetricPoints(projectID, metric, instanceName)
+
+				export(client.StartTime.In(client.Location()), projectID, metric, instanceName, points)
+			}
 		}
 	}
 }
