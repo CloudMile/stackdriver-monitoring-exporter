7faf3f270dd6fba7fcae6aff78261a856e446298
Bear Su <timfanda35@gmail.com>
2018-10-22 15:00:23 +0800

    Remove unuse code

--- main.go
+++ main.go
@@ -5,12 +5,6 @@ import (
 	"stackdriver-monitoring-exporter/pkg/service"
 )
 
-var monitoringMetrics = []string{
-	"compute.googleapis.com/instance/cpu/usage_time",
-	"compute.googleapis.com/instance/network/sent_bytes_count",
-	"compute.googleapis.com/instance/network/received_bytes_count",
-}
-
 func main() {
 	exportService := service.ExportService{}
 	exportService.Exporter = metric_exporter.FileExporter{}
