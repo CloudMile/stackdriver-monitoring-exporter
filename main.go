c1ce3fb57cf748d7fcb0575a60481b2f957edea2
Bear Su <timfanda35@gmail.com>
2018-10-26 14:43:11 +0800

    init GAE with runtime go111

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"stackdriver-monitoring-exporter/pkg/service"
 )
 
 func main() {
+	http.HandleFunc("/", indexHandler)
+
+	// [START setting_port]
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	// [END setting_port]
+}
+
+// indexHandler responds to requests with our greeting.
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	exportService := service.ExportService{}
 	exportService.Do()
+
+	fmt.Fprint(w, "Done")
 }
